677c898c22410271a77877c2fa2830749f5e566c
batan <122584289+batann@users.noreply.github.com>
2025-02-17 19:10:45 +1100

    Add files via upload

---
+++ plymouth/themes/install.sh
@@ -0,0 +1,25 @@
+# vim:fileencoding=utf-8:foldmethod=marker
+#!/bin/bash
+
+#{{{ >>>   Install plymouth
+lc-plymouth () {
+echo Please enter your sudo password if you are prompted to do so.
+echo Installing the lcanon theme...
+sudo mkdir /usr/share/plymouth/themes/lcanon
+sudo cp -rf ./ /usr/share/plymouth/themes/lcanon
+sudo update-alternatives --quiet --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/lcanon/lcanon.plymouth 100
+sudo update-alternatives --quiet --set default.plymouth /usr/share/plymouth/themes/lcanon/lcanon.plymouth
+sudo update-initramfs -u
+sudo update-grub2
+echo Done!
+echo Testing...
+sudo plymouthd
+sudo plymouth --show-splash
+sleep 10
+sudo plymouth quit
+echo Done!
+echo Have a nice day!
+
+}
+
+#}}}
